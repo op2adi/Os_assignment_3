30442292af8dec2b9508c79d00a116d64827d8c8
op2adi <adityaupadhyay2708@gmail.com>
2023-10-31 01:25:05 +0530

    completed

--- mems.h
+++ mems.h
@@ -1,102 +1,184 @@
-/*
-All the main functions with respect to the MeMS are inplemented here
-read the function discription for more details
-
-NOTE: DO NOT CHANGE THE NAME OR SIGNATURE OF FUNCTIONS ALREADY PROVIDED
-you are only allowed to implement the functions 
-you can also make additional helper functions a you wish
-
-REFER DOCUMENTATION FOR MORE DETAILS ON FUNSTIONS AND THEIR FUNCTIONALITY
-*/
-// add other headers as required
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
 #include <unistd.h>
-/*
-Use this macro where ever you need PAGE_SIZE.
-As PAGESIZE can differ system to system we should have flexibility to modify this 
-macro to make the output of all system same and conduct a fair evaluation. 
-*/
-#define PAGE_SIZE 4096
-struct main{
-    int page;
-    struct main * next;
-    struct main* prev;
+#include <sys/mman.h>
+
+#define PAGE_SIZE 4096  // This value might differ on some systems
+
+// MeMS data structures
+struct mems_subchain_node {
+    size_t size;
+    int is_hole;
+    struct mems_subchain_node* next;
+    struct mems_subchain_node* prev;
 };
 
+struct mems_mainchain_node {
+    size_t total_size;
+    struct mems_subchain_node* subchain_head;
+    struct mems_mainchain_node* next;
+    struct mems_mainchain_node* prev;
+};
 
-/*
-Initializes all the required parameters for the MeMS system. The main parameters to be initialized are:
-1. the head of the free list i.e. the pointer that points to the head of the free list
-2. the starting MeMS virtual address from which the heap in our MeMS virtual address space will start.
-3. any other global variable that you want for the MeMS implementation can be initialized here.
-Input Parameter: Nothing
-Returns: Nothing
-*/
-void mems_init(){
-    //  struct main* head;
+// Global variables
+struct mems_mainchain_node* mems_free_list = NULL;
+void* mems_heap_start = NULL;
+
+// Function to initialize the MeMS system
+void mems_init() {
+    // Initialize any global variables or data structures
+    mems_free_list = NULL;
+    
+    // Allocate an initial block of memory from the OS using mmap
+    size_t total_size = PAGE_SIZE;
+    mems_heap_start = mmap(NULL, total_size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+    
+    if (mems_heap_start != MAP_FAILED) {
+        struct mems_mainchain_node* initial_block = (struct mems_mainchain_node*)mems_heap_start;
+        initial_block->total_size = total_size;
+        initial_block->subchain_head = (struct mems_subchain_node*)((char*)initial_block + sizeof(struct mems_mainchain_node));
+        initial_block->subchain_head->size = total_size - sizeof(struct mems_subchain_node);
+        initial_block->subchain_head->is_hole = 1;
+        initial_block->subchain_head->next = NULL;
+        initial_block->subchain_head->prev = NULL;
+        initial_block->next = NULL;
+        initial_block->prev = NULL;
+        mems_free_list = initial_block;
+    }
+}
+
+// Function to allocate memory using MeMS
+void* mems_malloc(size_t size) {
+    if (size == 0) {
+        return NULL;  // Handle zero-size allocation
+    }
+
+    struct mems_mainchain_node* block = mems_free_list;
+    
+    while (block != NULL) {
+        struct mems_subchain_node* segment = block->subchain_head;
         
-}
+        while (segment != NULL) {
+            if (segment->is_hole && segment->size >= size) {
+                // Allocate memory from this segment
+                segment->is_hole = 0; // Mark it as a process
+                return (void*)((char*)block + sizeof(struct mems_mainchain_node) + (size_t)((char*)segment - (char*)mems_heap_start));
+            }
+            segment = segment->next;
+        }
+        
+        block = block->next;
+    }
 
+    // If no suitable segment is found, request more memory from the OS
+    size_t requested_size = size + sizeof(struct mems_subchain_node);
+    size_t num_pages = (requested_size + PAGE_SIZE - 1) / PAGE_SIZE;
+    size_t total_size = num_pages * PAGE_SIZE;
 
-/*
-This function will be called at the end of the MeMS system and its main job is to unmap the 
-allocated memory using the munmap system call.
-Input Parameter: Nothing
-Returns: Nothing
-*/
-void mems_finish(){
-}
+    // Use mmap to allocate more memory
+    struct mems_mainchain_node* new_block = (struct mems_mainchain_node*)mmap(NULL, total_size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
 
+    if (new_block != MAP_FAILED) {
+        new_block->total_size = total_size;
+        new_block->subchain_head = (struct mems_subchain_node*)((char*)new_block + sizeof(struct mems_mainchain_node));
+        new_block->subchain_head->size = total_size - sizeof(struct mems_subchain_node);
+        new_block->subchain_head->is_hole = 1;
+        new_block->subchain_head->next = NULL;
+        new_block->subchain_head->prev = NULL;
+        
+        // Add the new block to the free list
+        new_block->next = mems_free_list;
+        new_block->prev = NULL;
+        if (mems_free_list != NULL) {
+            mems_free_list->prev = new_block;
+        }
+        mems_free_list = new_block;
 
-/*
-Allocates memory of the specified size by reusing a segment from the free list if 
-a sufficiently large segment is available. 
-
-Else, uses the mmap system call to allocate more memory on the heap and updates 
-the free list accordingly.
-
-Note that while mapping using mmap do not forget to reuse the unused space from mapping
-by adding it to the free list.
-Parameter: The size of the memory the user program wants
-Returns: MeMS Virtual address (that is created by MeMS)
-*/ 
-void* mems_malloc(size_t size){
-
-}
-
-
-/*
-this function print the stats of the MeMS system like
-1. How many pages are utilised by using the mems_malloc
-2. how much memory is unused i.e. the memory that is in freelist and is not used.
-3. It also prints details about each node in the main chain and each segment (PROCESS or HOLE) in the sub-chain.
-Parameter: Nothing
-Returns: Nothing but should print the necessary information on STDOUT
-*/
-void mems_print_stats(){
-
-}
-
-
-/*
-Returns the MeMS physical address mapped to ptr ( ptr is MeMS virtual address).
-Parameter: MeMS Virtual address (that is created by MeMS)
-Returns: MeMS physical address mapped to the passed ptr (MeMS virtual address).
-*/
-void *mems_get(void*v_ptr){
+        // Try to allocate from the new block
+        return mems_malloc(size);
+    }
     
+    // Return NULL if mmap fails
+    return NULL;
 }
 
+// Function to deallocate memory
+void mems_free(void* ptr) {
+    if (ptr == NULL) {
+        return;  // Handle NULL pointer gracefully
+    }
 
-/*
-this function free up the memory pointed by our virtual_address and add it to the free list
-Parameter: MeMS Virtual address (that is created by MeMS) 
-Returns: nothing
-*/
-void mems_free(void *v_ptr){
+    // Find the block that contains the pointer
+    struct mems_mainchain_node* block = mems_free_list;
     
+    while (block != NULL) {
+        if ((ptr >= (void*)block) && (ptr < (void*)((char*)block + block->total_size))) {
+            // Calculate the offset within the block
+            size_t offset = (size_t)((char*)ptr - (char*)block - sizeof(struct mems_mainchain_node));
+            
+            // Find the corresponding segment
+            struct mems_subchain_node* segment = block->subchain_head;
+            while (segment != NULL) {
+                if (offset >= sizeof(struct mems_subchain_node) && offset < (size_t)((char*)segment - (char*)block)) {
+                    // Mark the segment as a hole
+                    segment->is_hole = 1;
+                    break;
+                }
+                segment = segment->next;
+            }
+            
+            break;
+        }
+        
+        block = block->next;
+    }
+}
+
+// Function to convert MeMS virtual address to MeMS physical address
+void* mems_get(void* v_ptr) {
+    struct mems_mainchain_node* block = mems_free_list;
+    
+    while (block != NULL) {
+        if ((v_ptr >= (void*)block) && (v_ptr < (void*)((char*)block + block->total_size))) {
+            // Calculate the offset within the block
+            size_t offset = (size_t)((char*)v_ptr - (char*)block - sizeof(struct mems_mainchain_node));
+            
+            // Calculate the physical address
+            return (void*)((char*)mems_heap_start + offset);
+        }
+        
+        block = block->next;
+    }
+    
+    return NULL;  // Address not found
+}
+
+// Function to print MeMS statistics
+void mems_print_stats() {
+    struct mems_mainchain_node* block = mems_free_list;
+    int block_count = 0;
+    int hole_count = 0;
+    int process_count = 0;
+    
+    while (block != NULL) {
+        block_count++;
+        
+        struct mems_subchain_node* segment = block->subchain_head;
+        
+        while (segment != NULL) {
+            if (segment->is_hole) {
+                hole_count++;
+            } else {
+                process_count++;
+            }
+            segment = segment->next;
+        }
+        
+        block = block->next;
+    }
+
+    printf("Total Mapped Pages: %d\n", block_count);
+    printf("Unused Memory (Bytes): %lu\n", (unsigned long)(hole_count * sizeof(struct mems_subchain_node)));
+    printf("Number of Holes: %d\n", hole_count);
+    printf("Number of Process Segments: %d\n", process_count);
 }
